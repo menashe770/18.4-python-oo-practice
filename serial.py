86a5b09192e531453b42a4f9473371c7a519069c
Menashe <mborukhov770@gmail.com>
2023-03-01 15:06:10 -0700

    serial

--- serial.py
+++ serial.py
@@ -21,23 +21,22 @@ class SerialGenerator:
     100
     """
 
-    # Define the constructor method for the class
     def __init__(self, start=0):
         # Initialize the start value and the next value to the start value
         self.start = self.next = start
 
-    # Define a method to return a string representation of the object
+    # Return a string representation of the object
     def __repr__(self):
-        # Return a formatted string with the start and next values
+        # Return a string with the start and next values
         return f"<SerialGenerator start={self.start} next={self.next}>"
 
-    # Define a method to generate the next serial number
+    # Generate the next serial number
     def generate(self):
         # increment the value of self.next by 1
         self.next += 1
         # Subtract 1 from the next value and return the result
         return self.next - 1
 
-    # Define a method to reset the next value to the start value
+    # Reset the next value to the start value
     def reset(self):
         self.next = self.start
